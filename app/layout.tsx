c10b2472d4c6cae04aa0a5802af0a65efcab4275
Tanja Schmidt <tagretha@gmail.com>
2023-09-25 12:44:57 +0200

    update configs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Link from "next/link";
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'FlickSpot',
   description: 'Find out about the latest movies',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <nav className="px-10 pt-5">
-         <Link prefetch href="/">
-           Flick<span className="text-amber-600">Spot</span>
-         </Link>
+        <nav className='px-10 pt-5'>
+          <Link prefetch href='/'>
+            Flick<span className='text-amber-600'>Spot</span>
+          </Link>
         </nav>
         {children}
       </body>
     </html>
-  )
+  );
 }
